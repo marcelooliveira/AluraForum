5cea024e3a786aac4ca2e45246f69f1e7a3ba594
marcelooliveira <mclricardo@gmail.com>
2017-01-10 16:47:58 -0200

    CCValadares
    CCValadares

---
+++ CCValadares/EntidadesContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCValadares
+{
+    public class EntidadesContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string stringConexao = ConfigurationManager.ConnectionStrings["MyDBConnectionString"].ConnectionString;
+            optionsBuilder.UseSqlServer(stringConexao);
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ CCValadares/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CCValadares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EntidadesContext contexto = new EntidadesContext();
+            Usuario renan = new Usuario()
+            {
+                Nome = "Renan",
+                Senha = "123"
+            };
+            contexto.Usuarios.Add(renan);
+            contexto.SaveChanges();
+            contexto.Dispose();
+        }
+    }
+}
